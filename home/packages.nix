52b5234e2710bab27d4858a018376e4b97f8f03f
Themos Papatheofanous <themos360@gmail.com>
2025-07-18 01:39:45 +0300

    psql

--- home/packages.nix
+++ home/packages.nix
@@ -56,6 +56,7 @@ in
     {
       # Some basics
       inherit (pkgs)
+        postgresql_16
         lld_20
         neofetch
         sqlx-cli #for interaction between rust back end and sql database
